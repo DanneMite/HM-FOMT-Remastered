f3d9e6985d43ebdc1858e94aa198c9085617ab57
Edsinger <danieledsinger@hotmail.com>
2015-12-09 21:47:43 +0100

    Test commit/push

--- HM FOMT Remastered/src/main/Main.java
+++ HM FOMT Remastered/src/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Harvest Moon!");
+		System.out.println("Hello Harvest Moon - Friends of Mineral Town!");
 	}
 
 }
